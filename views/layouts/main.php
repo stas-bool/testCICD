4c38b14110670545703596f890863a3fc0c4f6cf
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-03 23:48:28 +0700

    Final test 😀

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,6 +38,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Test', 'url' => ['/site/test']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
