fc88ec0913e15fc304d3a303769e8ff85c238c1f
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-06 13:50:38 +0700

    Исправление прав

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -11,8 +11,10 @@ fi
 ls /tmp/xdebug.log && chmod 766 /tmp/xdebug.log
 
 cd /var/www/cicd/ && \
-composer update && \
-#composer install && \
+chown -R www-data:www-data . && \
+find /var/www/ -type f -exec chmod 664 {} + && \
+find /var/www/ -type d -exec chmod 775 {} + && \
+composer install || composer update && \
 php yii migrate --interactive=0
 # Execute the CMD
 exec "$@"
