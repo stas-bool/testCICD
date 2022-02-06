9ff823dd8030bf6b1d601fa3bd2f42d163fdac6c
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-06 10:17:46 +0700

    Таблица для теста миграций

---
+++ models/Test.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property int $id
+ * @property string|null $value
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['value'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'value' => 'Value',
+        ];
+    }
+}
