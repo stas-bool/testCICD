fafd15394eaecd75d25a201edca366d3ea4af9ba
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-06 10:17:04 +0700

    Использование dotenv

--- config/db.php
+++ config/db.php
@@ -2,12 +2,17 @@
 
 use yii\db\Connection;
 
+$host = getenv("POSTGRES_HOST");
+$dbName = getenv("DB_NAME");
+$user = getenv("POSTGRES_USER");
+$password = getenv("POSTGRES_PASSWORD");
+
 return [
-    'class' => Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    "class" => Connection::class,
+    "dsn" => "pgsql:host=$host;dbname=$dbName",
+    "username" => $user,
+    "password" => $password,
+    "charset" => "utf8",
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
