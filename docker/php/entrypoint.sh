ce7e4d55bc27c1fbf097a456f05901f5c39cd73c
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-12 17:53:18 +0700

    Fix permissions in php/entrypoint.sh

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -9,7 +9,7 @@ uid=$(stat -c '%u' .)
 gid=$(stat -c '%g' .)
 usermod -u "$uid" $USER
 groupmod -g "$gid" $GROUP
-composer install || composer update && \
-php yii migrate --interactive=0
+sudo -u $USER composer install
+sudo -u $USER php yii migrate --interactive=0
 # Execute the CMD
 exec "$@"
