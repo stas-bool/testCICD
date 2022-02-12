92f9a614e0a07f08eab5bde7370dd6d5360a80b5
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-12 20:30:35 +0700

    Не выполнять миграции и не устанавливать зависимости при запуске контейнера

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -9,7 +9,5 @@ uid=$(stat -c '%u' .)
 gid=$(stat -c '%g' .)
 usermod -u "$uid" $USER
 groupmod -g "$gid" $GROUP
-sudo -u $USER composer install | sudo tee -a composer.log
-sudo -u $USER php yii migrate --interactive=0  | sudo tee -a migrate.log
 # Execute the CMD
 exec "$@"
