b0b3097e8d9ea343362d2119d6797028dbafbcc1
Stanislav Biche-ool <stas@biche-ool.ru>
2022-02-12 19:27:41 +0700

    Save composer install log

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -9,7 +9,7 @@ uid=$(stat -c '%u' .)
 gid=$(stat -c '%g' .)
 usermod -u "$uid" $USER
 groupmod -g "$gid" $GROUP
-sudo -u $USER composer install
-sudo -u $USER php yii migrate --interactive=0
+sudo -u $USER composer install | sudo tee -a composer.log
+sudo -u $USER php yii migrate --interactive=0  | sudo tee -a migrate.log
 # Execute the CMD
 exec "$@"
